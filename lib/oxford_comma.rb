5f49ee5c0ea298ead60c992d624b85cf756f0421
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 16:01:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.count == 1
+  return array.join
+elsif array.count==2 
+return array.join(" and ")
+else 
+  last = array.pop()
+  string =array.join(", ")
+  string << ", and "+ last
+end 
 end

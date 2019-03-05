8749c0227fb98a1d43e98b66a3607196330d75ad
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 16:01:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array.count == 1
   return array.join
 elsif array.count==2 
-return array.join("and")
+return array.join(" and ")
 else 
   last = array.pop()
   string =array.join(", ")
